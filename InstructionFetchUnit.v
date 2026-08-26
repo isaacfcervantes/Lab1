b5ccc2f70c1f18bc17523410b93ccf8368ac8c35
Isaac Cervantes <isaac.f.cervantes06@gmail.com>
2026-08-26 16:16:19 -0700

    Update InstructionFetchUnit.v

--- InstructionFetchUnit.v
+++ InstructionFetchUnit.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 ////////////////////////////////////////////////////////////////////////////////
-// Team Members:
+// Team Members: OEFINOEIGJEIOJIOJIOJIOJIO
 // Overall percent effort of each team meber: 
 // 
 // ECE369A - Computer Architecture
